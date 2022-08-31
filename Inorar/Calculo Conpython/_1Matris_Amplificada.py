5159fdcb0b16ff23ff8f811e323d295424f03a5d
calebsenm <calebgamercraft@gmail.com>
2022-08-30 20:46:06 -0500

    XD

---
+++ Inorar/Calculo Conpython/_1Matris_Amplificada.py
@@ -0,0 +1,35 @@
+# Crea el algoritmo de una matriz amplificada
+
+
+M = []
+
+while True:
+    Numero_Filas = int(input("Ingresa en numero de filas -> "))
+    Numero_Columnas = int(input("Ingresa el numero de Columnas -> "))
+
+    #llenado de la matriz
+
+    for i in range(Numero_Filas):
+        M.append([])
+        for j in range(Numero_Columnas):
+            M[i].append(0)
+
+            print(M[i][j],end = " ")
+        print()
+
+    for i in range(Numero_Filas):
+        for j in range(Numero_Columnas):
+            M[i][j] = int(input(f"ingresa el nunero de la posicion - {i+1} {j+1} -> "))
+        print()
+    
+    for i in range(Numero_Filas):
+        for j in range(Numero_Columnas):
+            
+            if j == Numero_Columnas -1:
+                print(f"|  {M[i][j]} ", end = " ")
+            else:
+                print(M[i][j],end = " ")
+        print()
+
+
+
