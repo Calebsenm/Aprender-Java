6b9f3b47134ba5c457cb3a46e5e0d535e56c942f
calebsenm <calebgamercraft@gmail.com>
2022-09-24 18:31:52 -0500

    Terminado

--- _2Ejercicios_De_Mintic2022/Actividad_5/Factura.java
+++ _2Ejercicios_De_Mintic2022/Actividad_5/Factura.java
@@ -1,52 +1,105 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Factura {
-    
+
     int Cantidad;
     String Descripcion;
     float Precio;
     float Valor_Venta;
+    Scanner io = new Scanner(System.in);
+    Cliente cliente;
+    ArrayList Producto_1;
 
-    public Factura(int Cantidad,String Descripcion, float Precio,float Valor_Venta){
+    public Factura(int Cantidad, String Descripcion, float Precio, float Valor_Venta, ArrayList Producto_1) {
 
         this.Cantidad = Cantidad;
         this.Descripcion = Descripcion;
         this.Precio = Precio;
         this.Valor_Venta = Valor_Venta;
+        this.Producto_1 = Producto_1;
+
     }
 
-    public void setCantidad(int Cantidad){
+    //
+    public void setCantidad(int Cantidad) {
         this.Cantidad = Cantidad;
     }
 
-    public void setDescripcion(String Descripcion){
+    public void setDescripcion(String Descripcion) {
         this.Descripcion = Descripcion;
     }
 
-    public void setPrecio(float Precio){
+    public void setPrecio(float Precio) {
         this.Precio = Precio;
     }
 
-    public void setDescripcion(float Valor_Venta){
+    public void setDescripcion(float Valor_Venta) {
         this.Valor_Venta = Valor_Venta;
+    }
+
+    public void AgregarProducto() {
+        
+        System.out.print("Ingresa un producto -> ");
+        String P = io.nextLine();
+        this.Producto_1.add(P);
+    }
+
+    public void RemoverProducto() {
+        System.out.print("Ingresa el nombre del producto que deseas remover -> ");
+        String Pp = io.nextLine();
+
+        for (int i = 0; i < Producto_1.size(); i++) {
+
+            String dato = (String) Producto_1.get(i);
+            if(Pp.equals(dato)) {
+                System.out.println(Producto_1.get(i) + " Ha sido removido correctamente");
+                Producto_1.remove(i);
+            }
+        }
+    }
+
+    public void EditarProducto() {
+        System.out.print("Ingresa un El producto que quiere  modificar -> ");
+        String P = io.nextLine();
+
+        for (int i = 0; i < Producto_1.size(); i++) {
+
+            String pro = (String) Producto_1.get(i); 
+            if (P.equals(pro)) {
+                System.out.print("Ingresa un El nuevo producto -> ");
+                String U = io.nextLine();
+                // this replace the element of the list
+                Producto_1.set(i,U);
+            }
+        }
+
+    }
+
+    // this if for print the count
+
+    public void printCount() {
+        // System.out.println("Este dato pertenese a " + cliente );
+        for (int i = 0; i < Producto_1.size(); i++) {
+            System.out.println(Producto_1.get(i));
+        }
     }
 
     // get
-    public int getCantidad(){
+    public int getCantidad() {
         return Cantidad;
     }
 
-    public String getDescripcion(){
+    public String getDescripcion() {
         return Descripcion;
     }
 
-    public float getPrecio(){
+    public float getPrecio() {
         return Precio;
     }
 
-    public float getValor_Venta(){
+    public float getValor_Venta() {
         return Valor_Venta;
     }
-
-
-
 
 }
--- _2Ejercicios_De_Mintic2022/Actividad_5/Main.java
+++ _2Ejercicios_De_Mintic2022/Actividad_5/Main.java
@@ -1,16 +1,26 @@
+import java.util.ArrayList;
 
-public class Main{
+public class Main {
     public static void main(String[] args) {
-        
 
-        
         Cliente cliente = new Cliente("Caleb", 17);
         Producto producto = new Producto(10, "Papas");
-        Factura factura = new Factura(22, "Pan con queso", 2, 123);
+        ArrayList producto1 = new ArrayList();
+        producto1.add("pan");
+        producto1.add("panes");
+        producto1.add("queso");
 
+        Factura factura = new Factura(30, "pa", 100, 2000, producto1);
+        while (true) {
+            System.out.print("\033[H\033[2J");
+            factura.printCount();
+            factura.AgregarProducto();
+            factura.EditarProducto();
+            System.out.print("\033[H\033[2J");
+            factura.printCount();
+            factura.RemoverProducto();
 
-        System.out.println(cliente.Edad);
-        System.out.println(producto.Nombre);
-        System.out.println(factura.Cantidad);
+        }
+
     }
 }
